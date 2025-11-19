9e27a182f6ed4d88241da5f1af750f983955ce79
shalinivan9669 <77225504+shalinivan9669@users.noreply.github.com>
2025-11-19 11:01:11 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -49,10 +49,7 @@
         '@': path.resolve(__dirname, './src'),
       },
     },
-    build: {
-      target: 'esnext',
-      outDir: 'build',
-    },
+   
     server: {
       port: 3000,
       open: true,
